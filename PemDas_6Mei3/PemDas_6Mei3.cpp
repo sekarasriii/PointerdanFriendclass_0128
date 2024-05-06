48c03c9cc2d24c3b81fcef8389ef2445293d6225
sekarasriii <sekareeesh@gmail.com>
2024-05-06 11:02:11 +0700

    masukkan prosedur void siswa

--- PemDas_6Mei3/PemDas_6Mei3.cpp
+++ PemDas_6Mei3/PemDas_6Mei3.cpp
@@ -23,3 +23,16 @@ public:
 	void setId(int pId);
 	void displayAll(orang& a);
 };
+
+void siswa::displayAll(orang& a) {
+	cout << id << endl
+		<< a.nama;
+}
+void orang::setNama(string pNama)
+{
+	nama = pNama;
+}
+void siswa::setId(int pId)
+{
+	id = pId;
+}
