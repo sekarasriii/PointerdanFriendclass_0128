725ed5cf258cc1fe6220d0cec0d709adb32fe36c
sekarasriii <sekareeesh@gmail.com>
2024-05-06 11:23:38 +0700

    membuat class mahasiswa berikut dengan akses modifier

---
+++ PemDas_6Mei4/PemDas_6Mei4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+private :
+	string nama;
+
+public:
+	friend void setNama(mahasiswa& a, string);
+};
+
+void setNama(mahasiswa& a, string pNama)
+{
+	a.nama = pNama;
+}
+
+int main()
+{
+	mahasiswa joko;
+	setNama(joko, "Kairi Kumat");
+	return 0;
+}
